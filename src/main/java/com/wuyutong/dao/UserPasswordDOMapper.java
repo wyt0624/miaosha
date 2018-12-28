1372a2db075c57b36a409b22b3cb23f5997a6b8e
Rzx <64537109@qq.com>
2018-12-28 17:27:42 +0800

    commit miaoshaProject

---
+++ src/main/java/com/wuyutong/dao/UserPasswordDOMapper.java
@@ -0,0 +1,30 @@
+package com.wuyutong.dao;
+
+import com.wuyutong.dataobject.UserPasswordDO;
+import com.wuyutong.dataobject.UserPasswordDOExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserPasswordDOMapper {
+    long countByExample(UserPasswordDOExample example);
+
+    int deleteByExample(UserPasswordDOExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserPasswordDO record);
+
+    int insertSelective(UserPasswordDO record);
+
+    List<UserPasswordDO> selectByExample(UserPasswordDOExample example);
+
+    UserPasswordDO selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") UserPasswordDO record, @Param("example") UserPasswordDOExample example);
+
+    int updateByExample(@Param("record") UserPasswordDO record, @Param("example") UserPasswordDOExample example);
+
+    int updateByPrimaryKeySelective(UserPasswordDO record);
+
+    int updateByPrimaryKey(UserPasswordDO record);
+}
