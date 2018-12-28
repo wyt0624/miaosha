76e217c8ef578f6a3f8e8fdb86d3f04981fe112c
wuyutong <64537109@qq.com>
2018-12-28 23:24:52 +0800

    NEW

--- src/main/java/com/wuyutong/App.java
+++ src/main/java/com/wuyutong/App.java
@@ -1,11 +1,13 @@
 package com.wuyutong;
 
 import com.wuyutong.dao.UserDOMapper;
+import com.wuyutong.dataobject.UserDO;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @MapperScan("com.wuyutong.dao")
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
 
+
+    @RequestMapping("/")
+    public String home(){
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null) {
+            return "用户不存在";
+        } else {
+            return userDO.getName();
+        }
+    }
 
     public static void main( String[] args ){
 
--- src/main/java/com/wuyutong/controller/UserController.java
+++ src/main/java/com/wuyutong/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.wuyutong.controller;
 
+import com.wuyutong.controller.viewobject.UserVO;
 import com.wuyutong.service.UserService;
 import com.wuyutong.service.model.UserModel;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +19,19 @@ public class UserController {
 
     @RequestMapping("/get")
     @ResponseBody
-    public UserModel getUser(@RequestParam(name="id")Integer id){
+    public UserVO getUser(@RequestParam(name="id")Integer id){
         //调用service
         UserModel userModel = userService.getUserById(id);
-        return userModel;
+        //将核心领域模型对象转化为可供UI使用的viewobject
+        return convertFromModel(userModel);
+    }
+
+    private UserVO convertFromModel(UserModel userModel){
+        if(userModel == null) {
+            return null;
+        }
+        UserVO userVO = new UserVO();
+        BeanUtils.copyProperties(userModel,userVO);
+        return userVO;
     }
 }
